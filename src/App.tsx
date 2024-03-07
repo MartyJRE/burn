661deba69324b8ba4927bfa2b809ae98fb2a18ae
Martin Pražák <martin.prazak@lutherone.com>
2024-03-07 14:10:06 +0100

    make it rain

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const averageMonthlySalary = 95_376;
 const monthInSeconds = 2_592_000;
 const averageSecondSalary = averageMonthlySalary / monthInSeconds;
 
-// create a timer, that ticks up every second
-// create a selector of number of developers that starts at 1
-// display the amount of money burned by the developers
-// create a button that resets the timer back to 0
+function formatTime(seconds: number): string {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+  return `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
 function BurnClock() {
   const [developers, setDevelopers] = useState(1);
   const [seconds, setSeconds] = useState(0);
   const [moneyBurned, setMoneyBurned] = useState(0);
 
-  // every second, add the amount of money burned by the developers to the total
-  // amount of money burned
   setTimeout(() => {
     setSeconds(seconds + 1);
     setMoneyBurned(seconds * developers * averageSecondSalary);
@@ -24,25 +25,46 @@ function BurnClock() {
   return (
     <div>
       <h1>Money Burned: CZK {moneyBurned.toFixed(2)}</h1>
-      <h1>Seconds of gitlab outage: {seconds}</h1>
+      <h1>Gitlab Outage time: {formatTime(seconds)}</h1>
       <div><p>Number of developers:</p>
       <input
         type="number"
         defaultValue="1"
-        onChange={(e) => setDevelopers(parseInt(e.target.value))}
+        onChange={(e) => setDevelopers(parseInt(e.target.value ?? 1))}
+        min="1"
+        required
       />
       </div>
     </div>
   );
-
 }
 
-function App() {
+function App() {  const [emojis, setEmojis] = useState<Array<{ top: number, left: number }>>([]);
+
+  useEffect(() => {
+    const initialEmojis = Array.from({ length: Math.floor(Math.random() * 50) + 50 }, () => ({
+      top: Math.random() * window.innerHeight,
+      left: Math.random() * window.innerWidth,
+    }));
+    setEmojis(initialEmojis);
+
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <h1>🔥</h1>
-        <BurnClock />
+        <BurnClock/>
+        {emojis.map((emoji, index) => (
+          <span
+            key={index}
+            className="money-emoji"
+            role="img"
+            aria-label="money"
+            style={{ position: 'absolute', top: emoji.top, left: emoji.left }}
+          >
+            💸
+          </span>
+        ))}
       </header>
     </div>
   );
